443c48e2756edea3411e38294ad9be9201d910ac
Hana Lee <i@leehana.co.kr>
2015-12-02 00:30:01 +0900

    로그인 및 유저 생성을 해주는 코드 추가.

---
+++ src/main/java/com/eyeq/SpaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.eyeq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaSpringApplication.class, args);
+    }
+}
